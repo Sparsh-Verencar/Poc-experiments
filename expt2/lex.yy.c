a0637c647456890918249350e88e8ed600384a45
Sparsh Verencar <verencarsparsh@gmail.com>
2026-02-02 17:46:24 +0530

    add exp7 folder and update exp2

--- expt2/lex.yy.c
+++ expt2/lex.yy.c
@@ -282,11 +282,11 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 3
-#define YY_END_OF_BUFFER 4
-static yyconst short int yy_accept[7] =
+#define YY_NUM_RULES 4
+#define YY_END_OF_BUFFER 5
+static yyconst short int yy_accept[9] =
     {   0,
-        0,    0,    4,    2,    1,    0
+        0,    0,    5,    3,    1,    2,    2,    0
     } ;
 
 static yyconst int yy_ec[256] =
@@ -297,14 +297,14 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    3,    3,    3,    3,    3,    3,
+        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
+        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
+        1,    1,    1,    1,    1,    1,    3,    3,    3,    3,
 
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
+        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
+        3,    3,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -321,29 +321,29 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[3] =
+static yyconst int yy_meta[4] =
     {   0,
-        1,    1
+        1,    1,    2
     } ;
 
-static yyconst short int yy_base[7] =
+static yyconst short int yy_base[10] =
     {   0,
-        0,    0,    3,    4,    4,    4
+        0,    0,    5,    6,    6,    0,    0,    6,    2
     } ;
 
-static yyconst short int yy_def[7] =
+static yyconst short int yy_def[10] =
     {   0,
-        6,    1,    6,    6,    6,    0
+        8,    1,    8,    8,    8,    9,    9,    0,    8
     } ;
 
-static yyconst short int yy_nxt[7] =
+static yyconst short int yy_nxt[10] =
     {   0,
-        4,    5,    6,    3,    6,    6
+        4,    5,    6,    7,    8,    3,    8,    8,    8
     } ;
 
-static yyconst short int yy_chk[7] =
+static yyconst short int yy_chk[10] =
     {   0,
-        1,    1,    3,    6,    6,    6
+        1,    1,    1,    9,    3,    8,    8,    8,    8
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -362,7 +362,8 @@ char *yytext;
 #line 2 "exp2.l"
 #include <stdio.h>
 int file_line_count = 0;
-#line 366 "lex.yy.c"
+int file_word_count = 0;
+#line 367 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -513,9 +514,9 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 6 "exp2.l"
+#line 7 "exp2.l"
 
-#line 519 "lex.yy.c"
+#line 520 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -566,13 +567,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 7 )
+				if ( yy_current_state >= 9 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 4 );
+		while ( yy_base[yy_current_state] != 6 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -600,24 +601,33 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 7 "exp2.l"
-{ file_line_count++; }
-	YY_BREAK
-case YY_STATE_EOF(INITIAL):
 #line 8 "exp2.l"
-{ printf("%d\n", file_line_count+1);return 0; }
+{ file_line_count++; }
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
 #line 9 "exp2.l"
-;
+{ printf("word found:%s\n", yytext); file_word_count++;}
+	YY_BREAK
+case YY_STATE_EOF(INITIAL):
+#line 10 "exp2.l"
+{ 
+    printf("file line count:%d\n", file_line_count+1);
+    printf("file word count:%d\n", file_word_count);
+    return 0; 
+}
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 10 "exp2.l"
+#line 15 "exp2.l"
+;
+	YY_BREAK
+case 4:
+YY_RULE_SETUP
+#line 16 "exp2.l"
 ECHO;
 	YY_BREAK
-#line 621 "lex.yy.c"
+#line 631 "lex.yy.c"
 
 	case YY_END_OF_BUFFER:
 		{
@@ -907,7 +917,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 7 )
+			if ( yy_current_state >= 9 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -942,11 +952,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 7 )
+		if ( yy_current_state >= 9 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 6);
+	yy_is_jam = (yy_current_state == 8);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1501,7 +1511,7 @@ int main()
 	return 0;
 	}
 #endif
-#line 10 "exp2.l"
+#line 16 "exp2.l"
 
 
 int yywrap() {
